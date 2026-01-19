aa1b6fec95b9d247a0f8a86bc650abee2b253a7a
Sanjay <sanjay06.ai@gmail.com>
2026-01-19 15:36:55 +0530

    Declared the input
    Prompt user for attendance percentage input.

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -4,5 +4,8 @@ Class AttendanceStatusEvaluator
   public static void main(String args[])
     {
     Scanner sc = new Scanner(System.in);
+    int percentage = sc.nextInt();
+    System.out.println("Enter the Attendance Percentage: ");
+    if (percentage >
     }
 }
